d0c42ca13691d3cc76ee7859b8bf6bbe024e16f7
ethanhinson <ethan@bluetent.com>
2016-05-12 22:08:39 -0600

    get extend hierarchy right

--- js/CacheInterface.js
+++ js/CacheInterface.js
@@ -4,6 +4,39 @@ CacheInterface.Class = function(attributes) {
   CacheInterface.extend(this, attributes);
 };
 
+CacheInterface.Class.extend = function (protoProps, staticProps) {
+  var parent = this;
+  var child;
+
+  // The constructor function for the new subclass is either defined by you
+  // (the "constructor" property in your `extend` definition), or defaulted
+  // by us to simply call the parent's constructor.
+  if (protoProps && Object.prototype.hasOwnProperty.call(protoProps, 'constructor')) {
+    child = protoProps.constructor;
+  } else {
+    child = function(){ return parent.apply(this, arguments); };
+  }
+
+  // Add static properties to the constructor function, if supplied.
+  CacheInterface.extend(child, parent, staticProps);
+
+  // Set the prototype chain to inherit from `parent`, without calling
+  // `parent`'s constructor function.
+  var Surrogate = function(){ this.constructor = child; };
+  Surrogate.prototype = parent.prototype;
+  child.prototype = new Surrogate;
+
+  // Add prototype properties (instance properties) to the subclass,
+  // if supplied.
+  if (protoProps) CacheInterface.extend(child.prototype, protoProps);
+
+  // Set a convenience property in case the parent's prototype is needed
+  // later.
+  child.__super__ = parent.prototype;
+
+  return child;
+};
+
 CacheInterface.extend = function (child) {
   // From _.extend
   var obj = Array.prototype.slice.call(arguments, 1);
@@ -39,14 +72,14 @@ CacheInterface.extend = function (child) {
 CacheInterface.Bucket = CacheInterface.Class.extend({
 
   constructor: function(atts) {
-    this.extend(atts, {
+    CacheInterface.extend(this, {
       defaultLifetime: 10,
       bucket: 'cache'
-    });
+    }, atts);
   },
 
   setItem: function(key, value, expires) {
-    
+
     if(!expires) {
       expires = this.defaultLifetime;
     }
